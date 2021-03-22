52b4499c4fa1ab17ec09b07891068e58f2b1e722
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-22 16:01:13 -0700

    fixed stack error

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,9 @@
 //Require Packages//
 const router = require("express").Router();
-const User = require("../../models/User.js");
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const AuthLockedRoute = require("./AuthLockedRoute.js");
+const AuthLockedRoute = require("./api-v1/AuthLockedRoute");
 
 //GET "/users" --test endpoint
 
