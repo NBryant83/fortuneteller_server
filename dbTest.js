f07007129029b7bb8016cef8db28eb5cb5b0c3ff
semuel1 <semuelst1@gmail.com>
2021-03-22 16:04:32 -0700

    tests added

--- dbTest.js
+++ dbTest.js
@@ -1,45 +1,47 @@
-const mongoose = require('mongoose')
-const User = require('./models/User')
-const UsersWisdom = require('./models/User')
-const Wisdom = require('./models/User')
-const models = require('./models')
-require('./models')
+//Require Packages//
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const UsersWisdom = require("./models/User");
+const Wisdom = require("./models/User");
+const models = require("./models");
+require("./models");
+
 // CREATE a user
 const userTest = async () => {
- try {
-  // CREATE a user
-  const newUser = new User({
-   username: 'test name',
-   password: 'password'
-  })
-  await newUser.save()
-  console.log('newUser', newUser)
-// READ a user
-  const foundUser = await User.findOne({
-   name: newUser.name
-  })
-// UPDATE a user
-  console.log('foundUser', foundUser)
-// DESTROY a user
+  try {
+    // CREATE a user
+    const newUser = new User({
+      username: "test name",
+      password: "password",
+    });
+    await newUser.save();
+    console.log("newUser", newUser);
 
-  // UPDATE a user
-  foundUser.name = 'test #2'
-  await foundUser.save()
-  const findUserAgain = await User.findOne({
-   name: 'bangBang'
-  })
-  console.log('findUserAgain', findUserAgain)
-  // DESTROY a user
-  const deleteUser = await User.deleteOne({
-   name: 'bangBang'
-  })
-  console.log('deleteUser', deleteUser)
-  // we done
-  process.exit()
- } catch (error) {
-  console.log(error)
-  process.exit()
- }
-}
-userTest()
+    // READ a user (find one)
+    const foundUser = await User.findOne({
+      name: newUser.name,
+    });
+    console.log("foundUser", foundUser);
+    foundUser.name = "test #2";
+    await foundUser.save();
 
+    //READ a user (find one again)
+    // const findUserAgain = await User.findOne({
+    //   name: "bangBang",
+    // });
+    // console.log("findUserAgain", findUserAgain);
+
+    // DESTROY a user
+    const deleteUser = await User.deleteOne({
+      name: "test name",
+    });
+    console.log("deleteUser", deleteUser);
+    process.exit();
+
+    //catch if error
+  } catch (error) {
+    console.log("👻 👻 👻 ", error);
+    process.exit();
+  }
+};
+// userTest();
