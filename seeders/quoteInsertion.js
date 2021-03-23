afe7dd0ee3e869a8384a288032b6e978ca26aebc
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-23 11:26:42 -0700

    getting brinns seeders route

--- seeders/quoteInsertion.js
+++ seeders/quoteInsertion.js
@@ -1,29 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Wisdom = require("../models/Wisdom");
 const quotes = require("./quotes.json");
 
-
 const addQuotes = () => {
-    try{
-        for(i = 0; i < quotes.quotes.length; i++){
-            // // console.log(quotes.quotes[i])
-            // const newWisdom = new Wisdom({
-            //     quote: quotes.quotes[i]
-            // })
-            // newWisdom.save()
-            Wisdom.create(quotes.quotes[i], (error, wisdom) => {
-                if(!error){
-                    console.log(wisdom)
-                } else{
-                    console.log(error)
-                }
-            })
-            // console.log("new quote", newWisdom)
+  try {
+    for (i = 0; i < quotes.quotes.length; i++) {
+      // // console.log(quotes.quotes[i])
+      // const newWisdom = new Wisdom({
+      //     quote: quotes.quotes[i]
+      // })
+      // newWisdom.save()
+      Wisdom.create(quotes.quotes[i], (error, wisdom) => {
+        if (!error) {
+          console.log(wisdom);
+        } else {
+          console.log(error);
         }
+      });
+      // console.log("new quote", newWisdom)
+    }
     // try{
     //     console.log(quotes.quotes[5])
-    } catch(error){
-        console.log(error)
-    }
-}
-addQuotes()
+  } catch (error) {
+    console.log(error);
+  }
+};
+// addQuotes()
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const rowdy = require("rowdy-logger");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose')
 //Config Express App//
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,11 +16,10 @@ app.use(morgan("tiny"));
 app.use(cors());
 //request body parser
 app.use(express.json());
-// mongoose.connect('mongodb://localhost/fortunetellerdb')
-
 
 //Mongoose Config//
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/fortunetellerdb";
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/fortunetellerdb";
 
 //Connect to MONGO_URI//
 mongoose.connect(MONGO_URI, {
@@ -38,6 +37,7 @@ db.once("open", () => {
 db.on("error", (err) => {
   console.error(`☠️ ☠️ ☠️ Oh no! Something is wrong with the DB!\n ${err}`);
 });
+
 //Custom Middleware//
 // app.use((req, res, next) => {
 //   console.log("Hello from a Middleware!🤘");
@@ -50,12 +50,10 @@ app.get("/", (req, res) => {
 });
 
 //Controllers//
-// app.use("/api-v1/users", require("./controllers/api-v1/usersController");
 app.use("/api-v1/users", require("./controllers/usersController"));
 
-app.get('/seed', (req, res) => {
-  
-})
+// app.get("/seed", (req, res) => {});
+
 //Tell Express to Listen on Port//
 app.listen(PORT, () => {
   rowdyResults.print();
