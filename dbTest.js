8f261f68d09d10f96c5e34bc050b12a9b6ee50e4
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-03-23 11:10:54 -0700

    trying to add seeders

--- dbTest.js
+++ dbTest.js
@@ -1,33 +1,33 @@
 //Require Packages//
-const mongoose = require("mongoose");
-const User = require("./models/User");
-const UsersWisdom = require("./models/User");
-const Wisdom = require("./models/User");
-const models = require("./models");
-require("./models");
+// const mongoose = require("mongoose");
+// const User = require("./models/User");
+// const UsersWisdom = require("./models/User");
+// const Wisdom = require("./models/User");
+// const models = require("./models");
+// require("./models");
 
-const quotes = require("./quotes.json");
+// const quotes = require("./quotes.json");
 
 //use quotes.quotes b/c it is inside a key value pair
 
 // CREATE a user
-const userTest = async () => {
-  try {
-    // CREATE a user
-    const newUser = new User({
-      username: "test name",
-      password: "password",
-    });
-    await newUser.save();
-    console.log("newUser", newUser);
+// const userTest = async () => {
+//   try {
+//     // CREATE a user
+//     const newUser = new User({
+//       username: "test name",
+//       password: "password",
+//     });
+//     await newUser.save();
+//     console.log("newUser", newUser);
 
-    // READ a user (find one)
-    const foundUser = await User.findOne({
-      name: newUser.name,
-    });
-    console.log("foundUser", foundUser);
-    foundUser.name = "test #2";
-    await foundUser.save();
+//     // READ a user (find one)
+//     const foundUser = await User.findOne({
+//       name: newUser.name,
+//     });
+//     console.log("foundUser", foundUser);
+//     foundUser.name = "test #2";
+//     await foundUser.save();
 
     //READ a user (find one again)
     // const findUserAgain = await User.findOne({
@@ -43,9 +43,9 @@ const userTest = async () => {
     // process.exit();
 
     //catch if error
-  } catch (error) {
-    console.log("👻 👻 👻 ", error);
-    process.exit();
-  }
-};
+//   } catch (error) {
+//     console.log("👻 👻 👻 ", error);
+//     process.exit();
+//   }
+// };
 // userTest();
